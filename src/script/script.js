795351b2b66e1373dcde21c55132ada7a5268eb8
cost-ezki <117600123+cost-ezki@users.noreply.github.com>
2025-01-30 20:44:51 +0200

    Prima versiune a proiectului

---
+++ src/script/script.js
@@ -0,0 +1,28 @@
+
+
+var numar1 = 5
+var numar2 = 10
+var rezultat = numar1 + numar2
+{console.log("Suma celor două numere este: " + rezultat);}
+
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+  }
+
+alert("Acceseaza inspect, pentru a vedea rezultatele din consola")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
